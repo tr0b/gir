dfda43f109c94d759c0fe80f107341674dd11d55
tr0b <trobin0000@gmail.com>
2020-08-09 12:58:34 -0600

    Gir - SSH Initial support -Added Initial support for SSH/HTTPS URL options when creating repo

--- gir.sh
+++ gir.sh
@@ -11,33 +11,42 @@ createrepo()
 	GIRINFO="**GIR: Git Repositories**\ngir allows you to create (currently) Github Repositories easily, from the command line.\nUsage:\ngir \$REPO_NAME \$GIT_USERNAME\nCreating a repository named foo with user bar\ngir 'foo' 'bar'\n"
 	# Check for necessary arguments, else return help screen
 	[[ $# -le 1 ]] && printf "%s $GIRINFO" && return 1
+	read -p "Do you want to establish SSH as your primary URL? (If not, HTTPS will be used) (y/n)" usessh
 	GIT_REPO=$1
 	GIT_USERNAME=$2
 	printf "Creating Github repository => '$GIT_REPO'...\n"
-	curl -u $GIT_USERNAME https://api.github.com/user/repos -d '{"name":"'$GIT_REPO'"}'
-	printf "Repo Created!"
-	read -p "Do you want to create an empty README file (y/n)" doreadme
-	[[ $doreadme == "y" ]] && createreadme && firstcommit || printf "Exiting..." && return 0 
+	curl -u $GIT_USERNAME https://api.github.com/user/repos -d '{"name":"'$GIT_REPO'"}' > /dev/null || printf "ERR: Unkown Error Ocurred"
+	printf "Repo Created!\n"
+	initrepo $usessh
+	read -p "Do you want to create an empty README file (y/n):" doreadme
+	[[ $doreadme == "y" ]] && createreadme || printf "Exiting..."; return 0 
 }
 
 createreadme(){
 	# Creates README.md
-	printf "Creating README ..."
+	printf "\nCreating README ..."
 	touch README.md
 	printf " done."
+	firstcommit
+
+}
+
+initrepo(){
+	# 
+	ORIGIN_URL="https://github.com/$GIT_USERNAME/$GIT_REPO.git"
+	git init
+	git remote add origin "$ORIGIN_URL"
+	[[ $1 == "y" ]] && git config --global url."git@github.com:".insteadOf "https://github.com/" && printf "Repo setup to use SSH Successfully!\n" || printf "Repo will use HTTPS\n"
 
 }
 
 firstcommit(){
 	# Performs first commit
-	ORIGIN_URL="https://github.com/$GIT_USERNAME/$GIT_REPO.git"
-	printf "Pushing to remote ..."
-	git init
+	printf "\nPushing to remote ..."
 	git add README.md
 	git commit -m "Initial Commit"
-	git remote add origin "$ORIGIN_URL"
 	git push -u origin master
-	printf " done.\nAccess Github Repository:\n$ORIGIN_URL" && return 0
+	printf " done.\nAccess Github Repository:\n$ORIGIN_URL\n"
 
 }
 createrepo $1 $2
